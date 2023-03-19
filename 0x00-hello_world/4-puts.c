8cf1166d69b3735165d2c05dbb53858e830dd20b
Naetan07 <nathannkume@gmail.com>
2023-03-19 15:35:19 -0700

    C commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /**
-*main - entry point
-*Description: 'This is C programming'
-*return: always 0 (success)
-*/
+ *main - entry point
+ *
+ *Description: 'This is C programming'
+ *
+ *return: always 0 (success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
